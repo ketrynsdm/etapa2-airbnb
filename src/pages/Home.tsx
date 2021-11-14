91ffe3b79ca813dc527afa86ef0dc8e271e9d747
Antonia Silva <antonia.josy95@gmail.com>
2021-11-14 15:36:57 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from '../styles/Home.module.css';
+
+function Home() {
+    return (
+       <div className={styles.container}>
+            <div>Header</div>
+            <div>Accomodation</div>
+            <div>Footer</div>
+       </div>
+    );
+}
+
+export default Home;
