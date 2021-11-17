dbccd3997bfb984e013c7608424cd451d8d76e45
Anderson Moura <andersonmouradasilva2@gmail.com>
2021-11-17 14:12:57 -0300

    autentificação implementada

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../components/Button';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 import '../assets/styles/authStyle.scss'
 
 export function Authentication() {
+    const [username, setusername] = useState('');
+    const [password, setpassword] = useState('');
+    const [isLogged, setIsLogged] = useState(false);
+    const api = axios.create({
+        baseURL: 'http://localhost:1234/api',
+        timeout: 1000,
+    }
+    );
+    const cookies = new Cookies();
+    const navigate = useNavigate();
+    useEffect(() => {
+        cookies.set('myCat', 'Pacman', { path: '/' });
+        console.log(cookies.get('myCat')); // Pacman
+    }, []);
     return (
         <div className="user-auth">
+
             <h1>Bem-vindo ao Airbnb</h1>
             <form autoComplete="nope">
                 <div className="form-control">
                     <label htmlFor="nome">nome</label>
-                    <input id="nome" type="text" name="nome" autoComplete="off" placeholder="Informe seu nome" />
-                    <label htmlFor="email">E-mail</label>
-                    <input id="email" type="text" name="email" autoComplete="off" placeholder="Informe seu email" />
+                    <input id="nome" type="text" name="nome" autoComplete="off" value={username} onChange={e => {
+                        setusername(e.target.value);
+                    }} placeholder="Informe seu nome de usuario" />
+                    {/* <label htmlFor="email">E-mail</label>
+                    <input id="email" type="text" name="email" autoComplete="off" placeholder="Informe seu email" /> */}
 
                     <label htmlFor="senha">Senha</label>
-                    <input id="senha" type="senha" name="senha" placeholder="Informe sua senha" />
+                    <input id="senha" type="password" name="senha" value={password} onChange={
+                        e => {
+                            setpassword(e.target.value);
+                            console.log({ username, password })
+                        }
+                    } placeholder="Informe sua senha" />
                 </div>
 
                 <div className="button-auth">
-                    <Button>
+                    <Button onClick={
+                        e => {
+                            e.preventDefault();
+                            api.post('/auth/signIn',
+                                {
+                                    nome: username,
+                                    senha: password
+                                }
+                            ).then(response => {
+                                cookies.set('authorization-token', response.data, { path: '/' })
+                                if (response.status == 200) {
+                                    navigate('/');
+                                }
+
+                            });
+                        }
+                    }>
                         Entrar
                     </Button>
                 </div>
-            </form>
-        </div>
+            </form >
+        </div >
     )
 }
